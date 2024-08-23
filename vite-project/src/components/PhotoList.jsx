d5e8aa8b35ef636bfcdc7cd56eb9758983446fcf
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-08-23 17:15:13 -0400

    ...

--- vite-project/src/components/PhotoList.jsx
+++ vite-project/src/components/PhotoList.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import Photo from "./Photo";
 
-const PhotoList = () => {
-
+function PhotoList({ photos, title }) {
     return (
-        <ul>
-           
-        </ul>
+      <div>
+        <h2>{title}</h2>
+        <div className="photo-container">
+          <ul>
+            {photos.map((photo) => (
+              <li key={photo.id}>
+                <Photo photo={photo} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     );
-}
+  }
 
 export default PhotoList;
