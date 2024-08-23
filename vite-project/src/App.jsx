a03c71e326c7646dd03d2ec06b19023180e714f3
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-08-23 14:22:08 -0400

    ..

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+//API
+import apiKey from './config';
+//Components
 import Nav from './components/Nav';
 import Search from './components/Search';
 import PhotoList from './components/PhotoList';
 
-
+function fetchData () {
+ const[images, SetImages] = useState()
+ useEffect(() => {
+  fetch("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=d653b043c3a66ddc4bf96656f192f529&tags=sunsets&per_page=24&format=json&nojsoncallback=1")
+ },[])
+}
 
 function App() {
   return (
